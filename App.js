47df204947dd47c3b5cf6b29ef81f121585bcf9a
Saiyyam Kochar <saiyyamkochar@gmail.com>
2020-12-19 18:18:25 -0500

    g

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Test } from "./frontend/test";
+import  Test  from "./frontend/test";
 
 export default function App() {
   return (
